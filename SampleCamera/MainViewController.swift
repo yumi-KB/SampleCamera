f160f88c81c801e72319340f6f8bb67fffa67620
yumi KB <bon.kyumi@gmail.com>
2020-11-26 16:00:50 +0900

    Create main view setting camera privacy

---
+++ SampleCamera/MainViewController.swift
@@ -0,0 +1,87 @@
+import UIKit
+
+class MainViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: Properties
+    
+    var photoImage: UIImage?
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoNaviLabel: UILabel!
+    
+    @IBAction func onCameraButtonTapped(_ sender: UIBarButtonItem) {
+    
+        let controller = UIAlertController(title: "", message: "どの方法で写真を読み込みますか？", preferredStyle: UIAlertController.Style.actionSheet)
+
+        controller.addAction(UIAlertAction(title: "写真を撮影する", style: .default, handler: { (action) in self.selectedCamera()} ))
+        controller.addAction(UIAlertAction(title: "カメラロールから読み込む", style: .default, handler: { (action) in self.selectedLibrary()} ))
+
+        controller.addAction(UIAlertAction(title: "キャンセル", style: .cancel, handler: nil))
+
+        controller.popoverPresentationController?.sourceView = self.view
+        controller.popoverPresentationController?.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
+        controller.popoverPresentationController?.permittedArrowDirections = []
+
+        self.present(controller, animated: true, completion: nil)
+
+    }
+    @IBAction func onSaveButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func onEditButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    // MARK: Methods
+    
+    func selectedCamera() {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .camera
+            picker.delegate = self
+            self.present(picker, animated: true, completion: nil)
+        }
+    }
+    
+    func selectedLibrary() {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .photoLibrary
+            picker.delegate = self
+            self.present(picker, animated: true, completion: nil)
+        }
+    }
+    
+    
+    // MARK: Delegate Methods
+    
+    private func imagePickerControllerDidCancel(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+        
+        if let editedImage: UIImage = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage {
+            photoImageView.image = editedImage
+            photoImage = editedImage
+        }
+        
+        if photoImageView.image != nil {
+            photoNaviLabel.isHidden = true
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+
+
+}
